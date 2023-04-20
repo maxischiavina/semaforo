b7dd3c73bcdd973469d9b3e5d9671645b1fd79f9
maxischiavina <maxischiavina@hotmail.com>
2023-04-20 09:23:07 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [rojo, setRojo] = useState("rojo")
+	const [amarillo, setAmarillo] = useState("amarillo")
+	const [verde, setVerde] = useState("verde")
+	const [luz, setLuz] = useState("border-white")
+
+	function cambiarEstadoRojo(){
+		setRojo("BRILLO"), setAmarillo("amarillo"), setVerde("verde");
+		}
+	function cambiarEstadoAmarillo(){
+		setAmarillo("BRILLO"), setRojo("rojo"), setVerde("verde");
+	}
+	function cambiarEstadoVerde(){
+		setVerde("BRILLO"), setRojo("rojo"), setAmarillo("amarillo");
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center align-items-center mt-5">
+		<div className="bg-dark rounded">
+			<div className="d-grid gap-3 col-1 mx-auto justify-content-center container">
+  			<button className={"rounded-circle bg-danger fs-1 " + luz} onClick={cambiarEstadoRojo}>{rojo}</button>
+  			<button className={"rounded-circle bg-warning fs-1 " + luz} onClick={cambiarEstadoAmarillo}>{amarillo}</button>
+			  <button className={"rounded-circle bg-success fs-1 " + luz} onClick={cambiarEstadoVerde}>{verde}</button>
+</div>
+		</div>
 		</div>
 	);
 };
