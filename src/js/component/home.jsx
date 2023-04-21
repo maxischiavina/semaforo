78b009f88dca268c264fb71c48bed910eca0d0a9
maxischiavina <maxischiavina@hotmail.com>
2023-04-21 08:55:11 +0000

    segundo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,22 +11,22 @@ const Home = () => {
 	const [luz, setLuz] = useState("border-white")
 
 	function cambiarEstadoRojo(){
-		setRojo("BRILLO"), setAmarillo("amarillo"), setVerde("verde");
+		setRojo("Brillo"), setAmarillo("amarillo"), setVerde("verde");
 		}
 	function cambiarEstadoAmarillo(){
-		setAmarillo("BRILLO"), setRojo("rojo"), setVerde("verde");
+		setAmarillo("Brillo"), setRojo("rojo"), setVerde("verde");
 	}
 	function cambiarEstadoVerde(){
-		setVerde("BRILLO"), setRojo("rojo"), setAmarillo("amarillo");
+		setVerde("Brillo"), setRojo("rojo"), setAmarillo("amarillo");
 	}
 
 	return (
 		<div className="d-flex justify-content-center align-items-center mt-5">
-		<div className="bg-dark rounded">
+		<div className="bg-dark rounded w-25" style={{maxWidth: "150px"}}>
 			<div className="d-grid gap-3 col-1 mx-auto justify-content-center container">
-  			<button className={"rounded-circle bg-danger fs-1 " + luz} onClick={cambiarEstadoRojo}>{rojo}</button>
-  			<button className={"rounded-circle bg-warning fs-1 " + luz} onClick={cambiarEstadoAmarillo}>{amarillo}</button>
-			  <button className={"rounded-circle bg-success fs-1 " + luz} onClick={cambiarEstadoVerde}>{verde}</button>
+		<button className={"rounded-circle bg-danger fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoRojo}>{rojo}</button>
+      <button className={"rounded-circle bg-warning fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoAmarillo}>{amarillo}</button>
+      <button className={"rounded-circle bg-success fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoVerde}>{verde}</button>
 </div>
 		</div>
 		</div>
