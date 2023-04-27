ccc9c793e451c8ce6595e66ee6ceac23018c771a
maxischiavina <maxischiavina@hotmail.com>
2023-04-27 14:41:31 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,28 +5,36 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [rojo, setRojo] = useState("rojo")
-	const [amarillo, setAmarillo] = useState("amarillo")
-	const [verde, setVerde] = useState("verde")
-	const [luz, setLuz] = useState("border-white")
+	const [rojo, setRojo] = useState("")
+	const [amarillo, setAmarillo] = useState("")
+	const [verde, setVerde] = useState("")
 
-	function cambiarEstadoRojo(){
-		setRojo("Brillo"), setAmarillo("amarillo"), setVerde("verde");
-		}
-	function cambiarEstadoAmarillo(){
-		setAmarillo("Brillo"), setRojo("rojo"), setVerde("verde");
-	}
-	function cambiarEstadoVerde(){
-		setVerde("Brillo"), setRojo("rojo"), setAmarillo("amarillo");
-	}
+function handleRed(){
+	setRojo("border-white");
+	setAmarillo("");
+	setVerde("");
+}
+
+function handleYellow(){
+	setRojo("");
+	setAmarillo("border-white");
+	setVerde("");
+}
+
+function handleGreen(){
+	setRojo("");
+	setAmarillo("");
+	setVerde("border-white");
+}
+
 
 	return (
 		<div className="d-flex justify-content-center align-items-center mt-5">
 		<div className="bg-dark rounded w-25" style={{maxWidth: "150px"}}>
 			<div className="d-grid gap-3 col-1 mx-auto justify-content-center container">
-		<button className={"rounded-circle bg-danger fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoRojo}>{rojo}</button>
-      <button className={"rounded-circle bg-warning fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoAmarillo}>{amarillo}</button>
-      <button className={"rounded-circle bg-success fs-1 " + luz} style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={cambiarEstadoVerde}>{verde}</button>
+		<button className={"rounded-circle bg-danger fs-1 " + rojo } style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={handleRed}></button>
+      <button className={"rounded-circle bg-warning fs-1 " + amarillo } style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={handleYellow}></button>
+      <button className={"rounded-circle bg-success fs-1 " + verde } style={{minWidth: "100px", maxWidth: "100", height: "100px"}} onClick={handleGreen}></button>
 </div>
 		</div>
 		</div>
